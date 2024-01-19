e3e4f94353d9f77bbb47f7015982cae5b1c30ac3
zow2023 <125123077+zow2023@users.noreply.github.com>
2024-01-19 09:49:27 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,6 +19,6 @@
 #git clone https://github.com/jerrykuku/lua-maxminddb.git
 #git clone https://github.com/jerrykuku/luci-app-vssr.git
 #rm -rf tmp && ./scripts/feeds install -f -p helloworld shadowsocksr-libev
-git clone https://github.com/kiddin9/luci-app-dnsfilter package/luic-app-dnsfilter
-git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
+#git clone https://github.com/kiddin9/luci-app-dnsfilter package/luic-app-dnsfilter
+#git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
 #git clone https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
