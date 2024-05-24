efdfd51beaae60822d9e32c596b6ba18dd2998f4
zow2023 <125123077+zow2023@users.noreply.github.com>
2024-05-24 16:07:36 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,6 +16,8 @@ git clone https://github.com/kiddin9/luci-app-dnsfilter package/luic-app-dnsfilt
 #git clone https://github.com/xiaorouji/openwrt-passwall.git -b  luci-smartdns-dev package/passwall/luci
 #git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall/luci
 
+rm -rf feeds/packages/net/adguardhome
+
 #git clone https://github.com/fw876/helloworld.git -b master package/helloworld
 
 #git clone https://github.com/sirpdboy/sirpdboy-package package/sirpdboy-package
