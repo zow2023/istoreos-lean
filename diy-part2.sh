8cc110c3b768bd0795dddaf2911cd30953af38b9
zow2023 <125123077+zow2023@users.noreply.github.com>
2024-05-27 17:16:31 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -23,15 +23,15 @@ git clone https://github.com/kiddin9/luci-app-dnsfilter package/luic-app-dnsfilt
 #git clone https://github.com/sirpdboy/sirpdboy-package package/sirpdboy-package
 #git clone https://github.com/sirpdboy/luci-app-parentcontrol package/luci-app-parentcontrol
 
-git clone https://github.com/kingyond/luci-app-accesscontrol-plus package/luci-app-accesscontrol-plus
+#git clone https://github.com/kingyond/luci-app-accesscontrol-plus package/luci-app-accesscontrol-plus
 
 #git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 #git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-theme-argon-config
 
 #sed -i 's/ +libopenssl-legacy//g' feeds/helloworld/shadowsocksr-libev/Makefile
 # 移除 openwrt feeds 自带的核心包
-rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box}
-git clone https://github.com/zow2023/openwrt_helloworld package/helloworld
+#rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box}
+#git clone https://github.com/zow2023/openwrt_helloworld package/helloworld
 
 # 更新 golang 1.22 版本
 #rm -rf feeds/packages/lang/golang
@@ -41,8 +41,8 @@ git clone https://github.com/zow2023/openwrt_helloworld package/helloworld
 
 #sed -i 's#GO_PKG_TARGET_VARS.*# #g' feeds/packages/utils/v2dat/Makefile
 
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+#rm -rf feeds/packages/lang/golang
+#git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 
 #rm -rf feeds/luci/applications/luci-app-mosdns
 #find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
